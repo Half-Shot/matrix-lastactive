276f7600452b303115bd3c4494d34df99ffafa0d
Half-Shot <will@half-shot.uk>
2019-11-14 13:34:48 +0000

    Tweak tests

--- src/test_lib.ts
+++ src/test_lib.ts
@@ -2,6 +2,8 @@ import { expect } from "chai";
 import { MatrixActivityTracker } from "./lib";
 import { MatrixPresence, WhoisInfo } from "matrix-bot-sdk";
 
+const TEST_USER = "@foobar:example.com";
+
 function createTracker(canUseWhois: boolean = false, presence?: MatrixPresence, whois?: WhoisInfo) {
     const tracker: any = new MatrixActivityTracker({
         homeserverUrl: "https://localhost",
@@ -44,75 +46,72 @@ describe("MatrixActivityTracker", () => {
         });
     });
     describe("isUserOnline", () => {
-        it("will enable whois if it can't be used", async () => {
-            // Set bumpLastActiveTime to return early.
+        it("will enable whois if it can be used", async () => {
             const {tracker} = createTracker(true);
-            tracker.bumpLastActiveTime("@foobar:example.com");
-            await tracker.isUserOnline("@foobar:example.com", 1000);
+            tracker.bumpLastActiveTime(TEST_USER);
+            await tracker.isUserOnline(TEST_USER, 1000);
             expect(tracker.usingWhois).to.be.true;
         });
         it("will disable whois if it can't be used", async () => {
-            // Set bumpLastActiveTime to return early.
             const {tracker} = createTracker(false);
-            tracker.bumpLastActiveTime("@foobar:example.com");
-            await tracker.isUserOnline("@foobar:example.com", 1000);
+            tracker.bumpLastActiveTime(TEST_USER);
+            await tracker.isUserOnline(TEST_USER, 1000);
             expect(tracker.usingWhois).to.be.false;
         });
         it("Will return online if user was bumped recently", async () => {
             const {tracker} = createTracker(false);
-            tracker.bumpLastActiveTime("@foobar:example.com");
-            const res = await tracker.isUserOnline("@foobar:example.com", 100);
+            tracker.bumpLastActiveTime(TEST_USER);
+            const res = await tracker.isUserOnline(TEST_USER, 100);
             expect(res.online).to.be.true;
             expect(res.inactiveMs).to.be.below(10);
         });
         it("will return online if presence is currently active", async () => {
-            // Set bumpLastActiveTime to return early.
             const {tracker} = createTracker(false, {
                 currently_active: true,
                 presence: "online"
             });
-            const res = await tracker.isUserOnline("@foobar:example.com", 1000);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.true;
             expect(res.inactiveMs).to.equal(0);
         });
         it("will return online if presence status is online", async () => {
-            // Set bumpLastActiveTime to return early.
             const {tracker} = createTracker(false, {
                 currently_active: false,
                 presence: "online"
             });
-            const res = await tracker.isUserOnline("@foobar:example.com", 1000);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.true;
             expect(res.inactiveMs).to.equal(0);
         });
         it("will return offline if presence last_active_ago > maxTime", async () => {
-            // Set bumpLastActiveTime to return early.
             const {tracker} = createTracker(false, {
                 currently_active: false,
                 presence: "offline",
                 last_active_ago: 1001
             });
-            const res = await tracker.isUserOnline("@foobar:example.com", 1000);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.false;
             expect(res.inactiveMs).to.equal(1001);
         });
         it("will return offline if canUseWhois is false and presence couldn't be used", async () => {
-            // Set bumpLastActiveTime to return early.
             const {tracker} = createTracker(false);
-            const res = await tracker.isUserOnline("@foobar:example.com", 1000);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.false;
             expect(res.inactiveMs).to.equal(-1);
         });
-        it("will return offline if user is remote and presence couldn't be used", async () => {
-            // Set bumpLastActiveTime to return early.
-            const {tracker} = createTracker(true);
-            const res = await tracker.isUserOnline("@foobar:notexample.com", 1000);
+        it("will return online if the user's time is set appropriately", async () => {
+            const {tracker} = createTracker(false);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.false;
             expect(res.inactiveMs).to.equal(-1);
+            const time = Date.now();
+            await tracker.setLastActiveTime(TEST_USER, time);
+            const res2 = await tracker.isUserOnline(TEST_USER, 1000);
+            expect(res2.online).to.be.true;
+            expect(res2.inactiveMs).to.be.lessThan(100); // Account for some time spent.
         });
         it("will return online if presence couldn't be used and a device was recently seen", async () => {
             const now = Date.now();
-            // Set bumpLastActiveTime to return early.
             const response: WhoisInfo = {
                 user_id: "@foobar:notexample.com",
                 devices: {
@@ -142,12 +141,11 @@ describe("MatrixActivityTracker", () => {
             };
             const {tracker} = createTracker(true, undefined, response);
 
-            const res = await tracker.isUserOnline("@foobar:example.com", 1000);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.true;
         });
         it("will return offline if presence couldn't be used and a device was not recently seen", async () => {
             const now = Date.now();
-            // Set bumpLastActiveTime to return early.
             const response: WhoisInfo = {
                 user_id: "@foobar:notexample.com",
                 devices: {
@@ -177,7 +175,7 @@ describe("MatrixActivityTracker", () => {
             };
             const {tracker} = createTracker(true, undefined, response);
 
-            const res = await tracker.isUserOnline("@foobar:example.com", 1000);
+            const res = await tracker.isUserOnline(TEST_USER, 1000);
             expect(res.online).to.be.false;
         });
     })
